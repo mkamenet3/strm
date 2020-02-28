22287680b7453c772df17d260cad0c6c29817df7
mkamenet3 <maria.kamenetsky@gmail.com>
2020-02-28 16:55:28 -0600

    added filter option using tidyeval

--- R/createlagvars.R
+++ R/createlagvars.R
@@ -7,22 +7,29 @@
 #' @param id Group identifier (example: state).
 #' @param time Number of time periods in the dataset. Lags will be taken for each time period. Default is 2 time periods. For a spatial-only regression model, set \code{time=1}. 
 #' @param trans Transformation to be applied. Current implementation allows for \code{"log"}, \code{"sqrt"}, \code{"log10"}, and transformations.
-createlagvars <- function(data, vars, id, time=2, trans=NULL){
+#' @example 
+#' \donttest{
+#'  res <- createlagvars(data = Produc, vars=c(y,xs), id="state", time=2, trans="log",year==1970 | year==1971)}
+createlagvars <- function(data, vars, id, time=2, trans=NULL, ...){
+    filter_args <- rlang::enquos(...)
     keepvars <- c(vars, id)
     id <- enquo(id)
     time <- time-1
     if(trans=="log"){
         outdf <- data %>%
+            dplyr::filter(!!!filter_args) %>%
             dplyr::select(!!keepvars) %>% 
             dplyr::mutate_at(vars(-!!id), .funs = list(log = ~log(.)))
     }
     else if (trans=="sqrt"){
         outdf <- data %>%
+            dplyr::filter(!!!filter_args) %>%
             dplyr::select(!!keepvars) %>% 
             dplyr::mutate_at(vars(-!!id), .funs = list(sqrt = ~sqrt(.)))
     }
     else if (trans=="log10"){
         outdf <- data %>%
+            dplyr::filter(!!!filter_args) %>%
             dplyr::select(!!keepvars) %>% 
             dplyr::mutate_at(vars(-!!id), .funs = list(log10 = ~log(.,10)))
     } 
@@ -42,26 +49,7 @@ createlagvars <- function(data, vars, id, time=2, trans=NULL){
     return(outdf)
 }
 
-#subdat <-  subset(Produc, year==1970 | year==1971)
-res <- createlagvars(data = subdat, 
-                     vars=c(y,xs), id="state", time=2, trans="log")
-                     #,
-                     #time=2, trans=NULL, filter=NULL)
+# res <- createlagvars(data = Produc, 
+#                      vars=c(y,xs), id="state", time=2, trans="log",
+#                      year==1970 | year==1971)
 
-
-
-
-
-
-suppressWarnings(Produc %>%
-                     dplyr::filter(year==1970|year==1971) %>%
-                     dplyr::select(gsp, pcap, pc, emp, unemp,state) %>%
-                     tidyr::nest(-state) %>%
-                     dplyr::mutate(lags = purrr::map(data, function(dat){
-                         purrr::imap_dfc(dat, ~set_names(map(1, lag, x= .x),
-                                                         paste0(.y, '_lag',1)))
-                     })) %>%
-                     tidyr::unnest(cols=c(data,lags)) %>%
-                     dplyr::filter(complete.cases(.))) %>%
-    #if(isTRUE(log))
-    dplyr::mutate_at(vars(-state), .funs = list(log = ~log(.,2)))
