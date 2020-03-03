a68468f06dc76ee18b0e06b27d8aa6531e367112
mkamenet3 <maria.kamenetsky@gmail.com>
2020-03-03 08:10:53 -0600

    added flexibility for wide/long formats

--- R/createlagvars.R
+++ R/createlagvars.R
@@ -7,15 +7,19 @@
 #' @param id Group identifier (example: state).
 #' @param time Number of time periods in the dataset. Lags will be taken for each time period. Default is 2 time periods. For a spatial-only regression model, set \code{time=1}. 
 #' @param trans Transformation to be applied. Current implementation allows for \code{"log"}, \code{"sqrt"}, \code{"log10"}, and transformations.
+#' @param wide Boolean indicator. Takes \code{TRUE} if data is in wide format and \code{FALSE} if data is in long format. Default is \code{FALSE}.
+#' @param ... Arguments to be passed to \code{dplyr::filter()}
 #' @example 
 #' \donttest{
 #'  outdf<- createlagvars(data = Produc, vars=c(y,xs), id="state", time=2, trans="log",year==1970 | year==1971)}
-createlagvars <- function(data, vars, id, time=2, trans=NULL, ...){
+createlagvars <- function(data, vars, id, time=2, trans, wide, ...){
+    print(1)
     filter_args <- rlang::enquos(...)
-    keepvars <- c(vars, id)
-    keepvars <- gsub("\\_.*$","",keepvars)
+    # keepvars <- c(vars, id)
+    # keepvars <- gsub("\\_.*$","",keepvars)
     #print(keepvars)
     id <- enquo(id)
+    print(2)
     time <- time-1
     if(trans=="log"){
         outdf <- data %>%
@@ -37,22 +41,33 @@ createlagvars <- function(data, vars, id, time=2, trans=NULL, ...){
     } 
     else if(trans==0){
         message("No transformations")
+        print(vars)
+        print(rlang::as_name(id))
+        keepvars <- c(vars, rlang::as_name(id))
+        print(keepvars)
+        #keepvars <- gsub("\\_.*$","",keepvars)
         outdf <- data %>%
             dplyr::filter(!!!filter_args) %>%
             dplyr::select(!!keepvars)
     } else {
         stop(paste0("Your selected transformation, ",trans, " is not yet implemented. Please create an issue."))
     } 
-    outdf <- suppressWarnings(outdf%>%
-        tidyr::nest(-!!id) %>%
-        dplyr::mutate(lags = purrr::map(data, function(dat){
-             purrr::imap_dfc(dat, ~set_names(map(time, lag, x= .x),
-                                             paste0('Tlag',time,".",.y)))
-                                             #paste0(.y, '_lag',time)))
-         })) %>%
-        tidyr::unnest(cols=c(data,lags)) %>%
-        dplyr::filter(complete.cases(.)))
-    return(outdf)
+    if (wide == FALSE){
+        outdf <- suppressWarnings(outdf%>%
+            tidyr::nest(-!!id) %>%
+            dplyr::mutate(lags = purrr::map(data, function(dat){
+                 purrr::imap_dfc(dat, ~set_names(map(time, lag, x= .x),
+                                                 paste0('Tlag',time,".",.y)))
+                                                 #paste0(.y, '_lag',time)))
+             })) %>%
+            tidyr::unnest(cols=c(data,lags)) %>%
+            dplyr::filter(complete.cases(.)))
+        return(outdf)
+    } else {
+        return(outdf)
+    }
+
+    #return(outdf)
 }
 
 
