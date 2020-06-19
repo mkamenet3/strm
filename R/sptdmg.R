4f208cc68f7fac40f0916c368efb0ba96500a76d
mkamenet3 <maria.kamenetsky@gmail.com>
2020-06-19 10:12:34 -0500

    added and documented sptdmg to package

---
+++ R/sptdmg.R
@@ -0,0 +1,20 @@
+#' sptdmg SpatialPolygonsDataFrame: Wisconsin Minor Civil Divisions
+#' 
+#' We use the example from *Spatial Regression Models for the Social Sciences* Chi & Zhu (2019). The example uses population growth data from 2000 to 2010. Data are at the minor civil division (MCD) level in Wisconsin. There are two years of data: 2000 and 2010. The variables we will use are:
+#' 
+#' * `LNP1000`: population growth from 2000 to 2010.
+#' * `LNP0090`: population growth from 1990 to 2000.
+#' * `POLD00`: percentage of the old population (age sixty-five and older) in 2000.
+#' * `POLD90`: percentage of the old population (age sixty-five and older) in 1990.
+#' 
+#' 
+#' @docType data
+#' @usage data(sptdmg)
+#' @keywords datasets
+#' @references Chi, Guangqing, and Jun Zhu. *Spatial Regression Models for the Social Sciences.* SAGE, 2019.
+#' 
+#' @examples 
+#' data(sptdmg)
+#' class(sptdmg)
+#' names(sptdmg)
+"sptdmg"
