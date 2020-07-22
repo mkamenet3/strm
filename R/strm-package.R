0f5ee4438a2b0ecc75f8e24dc8ca9f2f15c2c95f
mkamenet3 <maria.kamenetsky@gmail.com>
2020-07-22 13:27:37 -0500

    working on why linux distros fail rhub. One solution is including config.site file. I got a note with this. Solution for importing packages mentioned in note

--- R/strm-package.R
+++ R/strm-package.R
@@ -1,7 +1,18 @@
 #' strm: Spatio-temporal regression model
-#' 
+# Avoid CMD check note Namespace in Imports field not imported from
 #' @docType package
 #' @name strm
+#' @keywords internal
+#' @import graphics
+#' @import grDevices
+#' @import knitr
+#' @import lazyeval
+#' @import methods
+#' @import rgdal
+#' @import rmarkdown
+#' @import spdep
+#' @import testthat
+#' @import utils
 NULL
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
