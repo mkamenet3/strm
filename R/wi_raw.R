e21cc394ca1dfeda14d960cddf4e4d7fa49c659b
mkamenet3 <maria.kamenetsky@gmail.com>
2020-08-10 14:42:06 -0500

    added wi_raw.RData as package dataset instead for vignette working with census data

---
+++ R/wi_raw.R
@@ -0,0 +1,19 @@
+#' wi_raw sf dataframe: Wisconsin 5-Year County-Level Raw ACS Data
+#' 
+#' We use 5-year ACS county level data in Wisconsin downloaded using the tidycensus R package. The example uses raw 5-year estimates from 2010-2014, 2011-2015, 2012-2016, 2013-2017, 2014-2018 ACS data at the county-level in Wisconsin. The variables downloaded are:
+#'  \itemize{
+#'        \item `B17020_002` - Estimate: Total - Income in the past 12 months below poverty level
+#'        \item `B17020_001` - Estimate: Total - Poverty Status in the past 12 months.
+#'        \item `B23022_026` - Estimate: Total Female by Work Status by weeks worked in the past 12 months for the population 16-64 years old.
+#'        \item `B23022_001` - Estimate: Total: status in the past 12 months by usual hours worked per week in the past 12 months by weeks worked in the past 12 months for the population 16-64 years old (Male and Female)
+#' }
+#' @docType data
+#' @usage data(wi_raw)
+#' @keywords datasets
+#' @references Kyle Walker (2020). tidycensus: Load US Census Boundary and Attribute Data as 'tidyverse' and 'sf'-Ready Data Frames. R package version 0.9.9.5.https://CRAN.R-project.org/package=tidycensus
+#' @examples 
+#' @export
+#' data(wi_raw)
+#' class(wi_raw)
+#' names(wi_raw)
+"wi_raw"
