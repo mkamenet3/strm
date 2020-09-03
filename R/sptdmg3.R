fa5e8ea0c68f7c304bbe1ca4ba6ad82aa21a6bb9
mkamenet3 <maria.kamenetsky@gmail.com>
2020-09-03 10:50:30 -0500

    compressed data even more

--- R/sptdmg3.R
+++ R/sptdmg3.R
@@ -1,6 +1,6 @@
 #' sptdmg3 SpatialPolygonsDataFrame: Wisconsin Minor Civil Divisions
 #' 
-#' We use the example from *Spatial Regression Models for the Social Sciences* Chi & Zhu (2019). The example uses population growth data from 2000 to 2010. Data are at the minor civil division (MCD) level in Wisconsin. There are two years of data: 2000 and 2010. The variables we will use are:
+#' We use the example from Chi, G. and Zhu, J. (2019) Spatial Regression Models for the Social Sciences. The example uses population growth data from 2000 to 2010. Data are at the minor civil division (MCD) level in Wisconsin. There are two years of data: 2000 and 2010. The subset of variables we use are:
 #'  \itemize{
 #'      \item `LNP1000`: population growth from 2000 to 2010.
 #'      \item `LNP0090`: population growth from 1990 to 2000.
@@ -10,7 +10,7 @@
 #' @docType data
 #' @usage data(sptdmg3)
 #' @keywords datasets
-#' @references Chi, Guangqing, and Jun Zhu. *Spatial Regression Models for the Social Sciences.* SAGE, 2019.
+#' @references Chi, Guangqing, and Jun Zhu (2019). *Spatial Regression Models for the Social Sciences.* SAGE.
 #' @examples 
 #' data(sptdmg3)
 #' class(sptdmg3)
